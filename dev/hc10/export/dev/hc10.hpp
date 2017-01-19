785527e69123a6eb45f87f4fed4159b412848670
Max Payne <forgge@gmail.com>
2017-01-19 02:14:33 +0200

    tmp 2

--- dev/hc10/export/dev/hc10.hpp
+++ dev/hc10/export/dev/hc10.hpp
@@ -4,6 +4,9 @@
 #include <ecl/err.hpp>
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
+
+#include <algorithm>
 
 namespace ecl
 {
@@ -23,8 +26,66 @@ public:
     static err power_down();
 
 private:
-    static err xfer_common(const uint8_t *tx, uint8_t *rx, size_t sz);
+    static err send_recv(const char *cmd, const char *exp_resp, size_t cmd_size);
+    static err xfer_common(const uint8_t *tx, uint8_t *rx, size_t tx_sz, size_t rx_sz);
+
+    struct uart_guard
+    {
+        uart_guard() { Uart::lock(); }
+        ~uart_guard() { Uart::unlock(); }
+    };
 };
+
+//------------------------------------------------------------------------------
+
+template<typename Uart>
+err hc10_common<Uart>::init()
+{
+    constexpr char at_cmd[] = "AT";
+
+    err rc;
+
+    Uart::init();
+
+    // Locks UART
+    uart_guard guard;
+
+    if (is_error(rc = send_recv(at_cmd, "OK", sizeof(at_cmd)))) {
+        return rc;
+    }
+
+    return err::ok;
+}
+
+//------------------------------------------------------------------------------
+
+template<typename Uart>
+err hc10_common<Uart>::send_recv(const char *cmd, const char *exp_resp, size_t cmd_size)
+{
+    // Real response
+    char resp[16] = {0};
+
+    auto rc = xfer_common(reinterpret_cast<const uint8_t*>(cmd),
+                          reinterpret_cast<uint8_t*>(resp),
+                          cmd_size, sizeof(resp) - 1);
+
+    if (is_error(rc)) {
+        return rc;
+    }
+
+    if (strncmp(resp, exp_resp, std::min(sizeof(resp), cmd_size))) {
+        return err::inval;
+    }
+
+    return err::ok;
+}
+
+template<typename Uart>
+err hc10_common<Uart>::xfer_common(const uint8_t *tx, uint8_t *rx, size_t tx_sz, size_t rx_sz)
+{
+    Uart::set_buffers(tx, rx, tx_sz, rx_sz);
+    return Uart::xfer();
+}
 
 //------------------------------------------------------------------------------
 
@@ -41,7 +102,7 @@ public:
     static err xfer(uint8_t *rx, size_t sz);
 
 private:
-    static err xfer_async(const uint8_t *tx, uint8_t *rx, size_t sz);
+    static err xfer_async(const uint8_t *tx, uint8_t *rx, size_t tx_sz);
 };
 
 //------------------------------------------------------------------------------
--- dev/hc10/tests/hc10_unit.cpp
+++ dev/hc10/tests/hc10_unit.cpp
@@ -4,6 +4,89 @@
 #include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTestExt/MockSupport.h>
 
+// Error code helper
+// TODO: move it to 'utils' headers and protect with check of
+// current test state (enabled or disabled)
+static SimpleString StringFrom(ecl::err err)
+{
+    return SimpleString{ecl::err_to_str(err)};
+}
+
+
+
+//------------------------------------------------------------------------------
+// HC10 common mode mocks
+
+struct hc10_common_uart_mock
+{
+public:
+    static ecl::err init()
+    {
+        mock("uart_mock").actualCall("init");
+        return (ecl::err) mock("uart_mock").returnIntValueOrDefault((int)ecl::err::ok);
+    }
+
+    static void lock()
+    {
+        mock("uart_mock").actualCall("lock");
+        m_locked = true;
+    }
+
+    static void unlock()
+    {
+        mock("uart_mock").actualCall("unlock");
+        m_locked = false;
+    }
+
+    static ecl::err set_buffers(const uint8_t *tx, uint8_t *rx, size_t size)
+    {
+        mock("uart_mock").actualCall("set_buffers")
+                .withParameter("size", size)
+                .withStringParameter("tx", (const char*)tx)
+                .withPointerParameter("rx", rx);
+
+        return (ecl::err) mock("uart_mock").returnIntValueOrDefault((int)ecl::err::ok);
+    }
+
+    static ecl::err set_buffers(const uint8_t *tx, uint8_t *rx, size_t tx_sz, size_t rx_sz)
+    {
+        mock("uart_mock").actualCall("set_buffers")
+                .withStringParameter("tx", (const char *)tx);
+
+        m_tx = (const char*)tx;
+        m_rx = (char*)rx;
+        m_rx_sz = rx_sz;
+
+        return (ecl::err) mock("uart_mock").returnIntValueOrDefault((int)ecl::err::ok);
+    }
+
+    static ecl::err xfer(size_t *sent = nullptr, size_t *received = nullptr)
+    {
+        mock("uart_mock").actualCall("xfer").withOutputParameter("rx", m_rx);
+
+        return (ecl::err) mock("uart_mock").returnIntValueOrDefault((int)ecl::err::ok);
+    }
+
+    static void cleanup()
+    {
+        m_locked = false;
+        m_tx = nullptr;
+        m_rx = nullptr;
+        m_rx_sz = 0;
+    }
+
+    static const char   *m_tx;
+    static char         *m_rx;
+    static size_t       m_rx_sz;
+    static bool         m_locked;
+
+};
+
+const char *hc10_common_uart_mock::m_tx;
+char *hc10_common_uart_mock::m_rx;
+size_t hc10_common_uart_mock::m_rx_sz;
+bool hc10_common_uart_mock::m_locked;
+
 //------------------------------------------------------------------------------
 
 TEST_GROUP(hc10_common)
@@ -15,60 +98,54 @@ TEST_GROUP(hc10_common)
 
     void teardown()
     {
-
+        hc10_common_uart_mock::cleanup();
+        mock().clear();
     }
-};
-
-//------------------------------------------------------------------------------
-// HC10 common mode mocks
-
-struct hc10_common_uart_mock
-{
-public:
-    ecl::err init()
-    {
-        mock("uart_mock").actualCall("init");
-        return (ecl::err) mock("uart_mock").returnIntValueOrDefault((int)ecl::err::ok);
-    }
-
-    void lock()
-    {
-        mock("uart_mock").actualCall("lock");
-    }
-
-    void unlock()
-    {
-        mock("uart_mock").actualCall("unlock");
-    }
-
-    ecl::err set_buffers(const uint8_t *tx, uint8_t *rx, size_t size)
-    {
-        mock("uart_mock").actualCall("set_buffers")
-                .withParameter("size", size)
-                .withConstPointerParameter("tx", tx)
-                .withPointerParameter("rx", rx);
-
-        return (ecl::err) mock("uart_mock").returnIntValueOrDefault((int)ecl::err::ok);
-    }
-
-    ecl::err xfer(size_t *sent = nullptr, size_t *received = nullptr)
-    {
-        mock("uart_mock").actualCall("xfer")
-                .withPointerParameter("sent", sent)
-                .withPointerParameter("received", received);
-
-        return (ecl::err) mock("uart_mock").returnIntValueOrDefault((int)ecl::err::ok);
-    }
-
 };
 
 //------------------------------------------------------------------------------
 // HC-10 common mode tests
 
+using hc10_common = ecl::hc10_common<hc10_common_uart_mock>;
+
 TEST(hc10_common, init)
 {
+    const char valid_resp[] = "OK";
 
+    mock("uart_mock").expectOneCall("init");
+    mock("uart_mock").expectOneCall("lock");
+    mock("uart_mock").expectOneCall("unlock");
+    mock("uart_mock").expectOneCall("set_buffers")
+            .withStringParameter("tx", "AT");
+    mock("uart_mock").expectOneCall("xfer")
+            .withOutputParameterReturning("rx", valid_resp, sizeof(valid_resp));
+
+    auto rc = hc10_common::init();
+
+    CHECK_EQUAL(ecl::err::ok, rc);
+
+    mock().checkExpectations();
 }
+
+TEST(hc10_common, init_invalid_resp)
+{
+    const char invalid_resp[] = "BLEBLE";
+
+    mock("uart_mock").expectOneCall("init");
+    mock("uart_mock").expectOneCall("lock");
+    mock("uart_mock").expectOneCall("unlock");
+    mock("uart_mock").expectOneCall("set_buffers")
+            .withStringParameter("tx", "AT");
+    mock("uart_mock").expectOneCall("xfer")
+            .withOutputParameterReturning("rx", invalid_resp, sizeof(invalid_resp));
+
+    auto rc = hc10_common::init();
+
+    CHECK_EQUAL(ecl::err::inval, rc);
+
+    mock().checkExpectations();
+}
+
 
 int main(int argc, char *argv[])
 {
